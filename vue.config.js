a02119c2a75d977406933d60dd5a1a74008dc4e3
fxss <723107234@qq.com>
2020-06-03 22:18:20 +0800

    perf: :zap:Vue、vue-router、Vuex、axios、element-ui采用CDN的方式引入，添加gzip打包

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
 module.exports = {
   publicPath: '/vue-blog/',
   outputDir: 'docs',
@@ -17,6 +19,57 @@ module.exports = {
         ws: true,
         changeOrigin: true
       }
+    }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      var externals = {
+        vue: 'Vue',
+        axios: 'axios',
+        'element-ui': 'ELEMENT',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex'
+      }
+      config.externals(externals)
+      const cdn = {
+        css: [
+          // element-ui css
+          'https://cdn.bootcdn.net/ajax/libs/element-ui/2.4.5/theme-chalk/index.css'
+        ],
+        js: [
+          // vue
+          'https://cdn.bootcdn.net/ajax/libs/vue/2.6.10/vue.min.js',
+          // vue-router
+          'https://cdn.bootcdn.net/ajax/libs/vue-router/3.0.6/vue-router.min.js',
+          // vuex
+          'https://cdn.bootcdn.net/ajax/libs/vuex/3.1.2/vuex.min.js',
+          // axios
+          'https://cdn.bootcdn.net/ajax/libs/axios/0.18.0/axios.min.js',
+          // element-ui js
+          'https://cdn.bootcdn.net/ajax/libs/element-ui/2.4.5/index.js'
+        ]
+      }
+      config.plugin('html')
+        .tap(args => {
+          args[0].cdn = cdn
+          return args
+        })
+    }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 生产环境
+      config.plugins.push(
+        new CompressionPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      )
+    } else {
+      // 开发环境
     }
   }
 }
